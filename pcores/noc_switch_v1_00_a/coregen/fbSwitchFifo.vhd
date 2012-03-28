0563930e10a6ca0c676afbea7356b625e7e6af76
Richard Huber <rihuber@ee.ethz.ch>
2012-03-28 23:38:26 +0200

     Created new coregen projects with common and individual rd/wr clocks

--- pcores/noc_switch_v1_00_a/coregen/fbSwitchFifo.vhd
+++ pcores/noc_switch_v1_00_a/coregen/fbSwitchFifo.vhd
@@ -26,8 +26,8 @@
 --    All rights reserved.                                                    --
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
--- You must compile the wrapper file interSwitchFifo.vhd when simulating
--- the core, interSwitchFifo. When compiling the wrapper file, be sure to
+-- You must compile the wrapper file fbSwitchFifo.vhd when simulating
+-- the core, fbSwitchFifo. When compiling the wrapper file, be sure to
 -- reference the XilinxCoreLib VHDL simulation library. For detailed
 -- instructions, please refer to the "CORE Generator Help".
 
@@ -40,10 +40,11 @@ USE ieee.std_logic_1164.ALL;
 -- synthesis translate_off
 LIBRARY XilinxCoreLib;
 -- synthesis translate_on
-ENTITY interSwitchFifo IS
+ENTITY fbSwitchFifo IS
   PORT (
-    clk : IN STD_LOGIC;
     rst : IN STD_LOGIC;
+    wr_clk : IN STD_LOGIC;
+    rd_clk : IN STD_LOGIC;
     din : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
     wr_en : IN STD_LOGIC;
     rd_en : IN STD_LOGIC;
@@ -51,14 +52,15 @@ ENTITY interSwitchFifo IS
     full : OUT STD_LOGIC;
     empty : OUT STD_LOGIC
   );
-END interSwitchFifo;
+END fbSwitchFifo;
 
-ARCHITECTURE interSwitchFifo_a OF interSwitchFifo IS
+ARCHITECTURE fbSwitchFifo_a OF fbSwitchFifo IS
 -- synthesis translate_off
-COMPONENT wrapped_interSwitchFifo
+COMPONENT wrapped_fbSwitchFifo
   PORT (
-    clk : IN STD_LOGIC;
     rst : IN STD_LOGIC;
+    wr_clk : IN STD_LOGIC;
+    rd_clk : IN STD_LOGIC;
     din : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
     wr_en : IN STD_LOGIC;
     rd_en : IN STD_LOGIC;
@@ -69,7 +71,7 @@ COMPONENT wrapped_interSwitchFifo
 END COMPONENT;
 
 -- Configuration specification
-  FOR ALL : wrapped_interSwitchFifo USE ENTITY XilinxCoreLib.fifo_generator_v8_3(behavioral)
+  FOR ALL : wrapped_fbSwitchFifo USE ENTITY XilinxCoreLib.fifo_generator_v8_3(behavioral)
     GENERIC MAP (
       c_add_ngc_constraint => 0,
       c_application_type_axis => 0,
@@ -94,9 +96,9 @@ END COMPONENT;
       c_axis_tstrb_width => 4,
       c_axis_tuser_width => 4,
       c_axis_type => 0,
-      c_common_clock => 1,
+      c_common_clock => 0,
       c_count_type => 0,
-      c_data_count_width => 11,
+      c_data_count_width => 4,
       c_default_value => "BlankString",
       c_din_width => 9,
       c_din_width_axis => 1,
@@ -163,7 +165,7 @@ END COMPONENT;
       c_has_wr_ack => 0,
       c_has_wr_data_count => 0,
       c_has_wr_rst => 0,
-      c_implementation_type => 0,
+      c_implementation_type => 2,
       c_implementation_type_axis => 1,
       c_implementation_type_rach => 1,
       c_implementation_type_rdch => 1,
@@ -179,7 +181,7 @@ END COMPONENT;
       c_overflow_low => 0,
       c_preload_latency => 0,
       c_preload_regs => 1,
-      c_prim_fifo_type => "1kx18",
+      c_prim_fifo_type => "512x36",
       c_prog_empty_thresh_assert_val => 4,
       c_prog_empty_thresh_assert_val_axis => 1022,
       c_prog_empty_thresh_assert_val_rach => 1022,
@@ -195,14 +197,14 @@ END COMPONENT;
       c_prog_empty_type_wach => 5,
       c_prog_empty_type_wdch => 5,
       c_prog_empty_type_wrch => 5,
-      c_prog_full_thresh_assert_val => 1023,
+      c_prog_full_thresh_assert_val => 15,
       c_prog_full_thresh_assert_val_axis => 1023,
       c_prog_full_thresh_assert_val_rach => 1023,
       c_prog_full_thresh_assert_val_rdch => 1023,
       c_prog_full_thresh_assert_val_wach => 1023,
       c_prog_full_thresh_assert_val_wdch => 1023,
       c_prog_full_thresh_assert_val_wrch => 1023,
-      c_prog_full_thresh_negate_val => 1022,
+      c_prog_full_thresh_negate_val => 14,
       c_prog_full_type => 0,
       c_prog_full_type_axis => 5,
       c_prog_full_type_rach => 5,
@@ -211,10 +213,10 @@ END COMPONENT;
       c_prog_full_type_wdch => 5,
       c_prog_full_type_wrch => 5,
       c_rach_type => 0,
-      c_rd_data_count_width => 11,
-      c_rd_depth => 1024,
+      c_rd_data_count_width => 4,
+      c_rd_depth => 16,
       c_rd_freq => 1,
-      c_rd_pntr_width => 10,
+      c_rd_pntr_width => 4,
       c_rdch_type => 0,
       c_reg_slice_mode_axis => 0,
       c_reg_slice_mode_rach => 0,
@@ -236,13 +238,13 @@ END COMPONENT;
       c_use_ecc_wrch => 0,
       c_use_embedded_reg => 0,
       c_use_fifo16_flags => 0,
-      c_use_fwft_data_count => 1,
+      c_use_fwft_data_count => 0,
       c_valid_low => 0,
       c_wach_type => 0,
       c_wdch_type => 0,
       c_wr_ack_low => 0,
-      c_wr_data_count_width => 11,
-      c_wr_depth => 1024,
+      c_wr_data_count_width => 4,
+      c_wr_depth => 16,
       c_wr_depth_axis => 1024,
       c_wr_depth_rach => 16,
       c_wr_depth_rdch => 1024,
@@ -250,7 +252,7 @@ END COMPONENT;
       c_wr_depth_wdch => 1024,
       c_wr_depth_wrch => 16,
       c_wr_freq => 1,
-      c_wr_pntr_width => 10,
+      c_wr_pntr_width => 4,
       c_wr_pntr_width_axis => 10,
       c_wr_pntr_width_rach => 4,
       c_wr_pntr_width_rdch => 10,
@@ -263,10 +265,11 @@ END COMPONENT;
 -- synthesis translate_on
 BEGIN
 -- synthesis translate_off
-U0 : wrapped_interSwitchFifo
+U0 : wrapped_fbSwitchFifo
   PORT MAP (
-    clk => clk,
     rst => rst,
+    wr_clk => wr_clk,
+    rd_clk => rd_clk,
     din => din,
     wr_en => wr_en,
     rd_en => rd_en,
@@ -276,4 +279,4 @@ U0 : wrapped_interSwitchFifo
   );
 -- synthesis translate_on
 
-END interSwitchFifo_a;
+END fbSwitchFifo_a;
