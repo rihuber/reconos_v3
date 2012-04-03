c2cdcc08d82a9cf737eabe46fe35bd6d1d6eaf86
Richard Huber <rihuber@ee.ethz.ch>
2012-04-03 11:06:13 +0200

    Bugfix: Double intverted signal

--- pcores/noc_switch_v1_00_a/hdl/vhdl/noc_switch.vhd
+++ pcores/noc_switch_v1_00_a/hdl/vhdl/noc_switch.vhd
@@ -37,7 +37,6 @@ entity noc_switch is
 		ringInputEmpty			: in std_logic_vector(numExtPorts-1 downto 0);
 		ringInputData			: in std_logic_vector((numExtPorts*(dataWidth+1))-1 downto 0);
 		ringInputReadEnable		: out std_logic_vector(numExtPorts-1 downto 0);
-		
 		ringOutputReadEnable	: in std_logic_vector(numExtPorts-1 downto 0);
 		ringOutputData			: out std_logic_vector((numExtPorts*(dataWidth+1))-1 downto 0);
 		ringOutputEmpty			: out std_logic_vector(numExtPorts-1 downto 0)
@@ -62,6 +61,26 @@ architecture rtl of noc_switch is
 	signal outputBufferOut	: inputLinkInArray(numPorts-1 downto 0);
 	signal outputBufferIn	: inputLinkOutArray(numPorts-1 downto 0);
 	
+	signal loc_upstream0ReadEnable : std_logic;
+	signal loc_upstream0Data : std_logic_vector(8 downto 0);
+	signal loc_upstream0Empty : std_logic;
+	signal loc_upstream1ReadEnable : std_logic;
+	signal loc_upstream1Data : std_logic_vector(8 downto 0);
+	signal loc_upstream1Empty : std_logic;
+	signal loc_downstream0WriteEnable : std_logic;
+	signal loc_downstream0Full : std_logic;
+	signal loc_downstream0Data: std_logic_vector(8 downto 0);
+	signal loc_downstream1WriteEnable : std_logic;
+	signal loc_downstream1Full : std_logic;
+	signal loc_downstream1Data : std_logic_vector(8 downto 0);
+	signal loc_ringDataIn : std_logic_vector(8 downto 0);
+	signal loc_ringDataOut : std_logic_vector(8 downto 0);
+	signal loc_ringWriteEnable : std_logic;
+	signal loc_ringReadEnable : std_logic;
+	signal loc_ringEmpty : std_logic;
+	signal loc_ringFull : std_logic;
+	
+	                   
 	component fbSwitchFifo
 		port (
 		rst: IN std_logic;
@@ -76,31 +95,22 @@ architecture rtl of noc_switch is
 	);
 	end component;
 	
-	COMPONENT interSwitchFifo
-	  PORT (
-	    clk : IN STD_LOGIC;
-	    rst : IN STD_LOGIC;
-	    din : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
-	    wr_en : IN STD_LOGIC;
-	    rd_en : IN STD_LOGIC;
-	    dout : OUT STD_LOGIC_VECTOR(8 DOWNTO 0);
-	    full : OUT STD_LOGIC;
-	    empty : OUT STD_LOGIC
-	  );
-END COMPONENT;
 	
 begin
-
-	-----------------------------------------------------------------
-	-- INPUT FROM RING
-	-----------------------------------------------------------------
 	
-	ring_input_generate : for i in numPorts-1 downto numIntPorts generate
-		swInputLinksIn(i).empty <= ringInputEmpty(i-numIntPorts);
-		swInputLinksIn(i).data <= ringInputData((i-numIntPorts+1)*(dataWidth+1)-1 downto (i-numIntPorts)*(dataWidth+1));
-		ringInputReadEnable(i-numIntPorts) <= swInputLinksOut(i).readEnable;
-	end generate;
-
+	loc_upstream0ReadEnable <= not loc_downstream1Full;
+	loc_upstream1ReadEnable <= not loc_ringFull;
+	loc_downstream0WriteEnable <= not ringInputEmpty(0);
+	loc_downstream0Data <= ringInputData(8 downto 0);
+	loc_downstream1WriteEnable <= not loc_upstream0Empty;
+	loc_downstream1Data <= loc_upstream0Data;
+	loc_ringDataIn <= loc_upstream1Data;
+	loc_ringWriteEnable <= not loc_upstream1Empty;
+	loc_ringReadEnable <= ringOutputReadEnable(0);
+	
+	ringOutputData(8 downto 0) <= loc_ringDataOut;
+	ringOutputEmpty(0) <= loc_ringEmpty;
+	ringInputReadEnable(0) <= not loc_downstream0Full;
 	
 	-----------------------------------------------------------------
 	-- INPUT FROM FUNCTIONAL BLOCK
@@ -113,10 +123,10 @@ begin
 			wr_clk => upstream0WriteClock,
 			din => upstream0Data,
 			wr_en => upstream0WriteEnable,
-			rd_en => swInputLinksOut(0).readEnable,
-			dout => swInputLinksIn(0).data,
+			rd_en => loc_upstream0ReadEnable,
+			dout => loc_upstream0Data,
 			full => upstream0Full,
-			empty => swInputLinksIn(0).empty
+			empty => loc_upstream0Empty
 		);
 	
 	fifo_upstream1 : fbSwitchFifo
@@ -126,97 +136,52 @@ begin
 			wr_clk => upstream1WriteClock,
 			din => upstream1Data,
 			wr_en => upstream1WriteEnable,
-			rd_en => swInputLinksOut(1).readEnable,
-			dout => swInputLinksIn(1).data,
+			rd_en => loc_upstream1ReadEnable,
+			dout => loc_upstream1Data,
 			full => upstream1Full,
-			empty => swInputLinksIn(1).empty
+			empty => loc_upstream1Empty
+		);
+		
+	fifo_downstream0 : fbSwitchFifo
+		port map (
+			rst => reset,
+			rd_clk => downstream0ReadClock,
+			wr_clk => clk125,
+			din => loc_downstream0Data,
+			wr_en => loc_downstream0WriteEnable,
+			rd_en => downstream0ReadEnable,
+			dout => downstream0Data,
+			full => loc_downstream0Full,
+			empty => downstream0Empty
+		);
+	
+	fifo_downstream1 : fbSwitchFifo
+		port map (
+			rst => reset,
+			rd_clk => downstream1ReadClock,
+			wr_clk => clk125,
+			din => loc_downstream1Data,
+			wr_en => loc_downstream1WriteEnable,
+			rd_en => downstream1ReadEnable,
+			dout => downstream1Data,
+			full => loc_downstream1Full,
+			empty => downstream1Empty
+		);
+		
+	fifo_ring : fbSwitchFifo
+		port map (
+			rst => reset,
+			rd_clk => clk125,
+			wr_clk => clk125,
+			din => loc_ringDataIn,
+			wr_en => loc_ringWriteEnable,
+			rd_en => loc_ringReadEnable,
+			dout => loc_ringDataOut,
+			full => loc_ringFull,
+			empty => loc_ringEmpty
 		);
 
 		
-	-----------------------------------------------------------------
-	-- SWITCH
-	-----------------------------------------------------------------
 	
-	sw : entity noc_switch_v1_00_a.switch
-		generic map(
-			globalAddress => globalAddr
-		)
-		port map(
-			clk		=> clk125,
-			reset		=> reset,
-			inputLinksIn	=> swInputLinksIn,
-			inputLinksOut	=> swInputLinksOut,
-			outputLinksIn	=> swOutputLinksIn,
-			outputLinksOut	=> swOutputLinksOut
-		);
-		
-		
-	-----------------------------------------------------------------
-	-- OUTPUT BUFFER
-	-----------------------------------------------------------------
-	
-	generate_output_buffer_fifo: for i in numIntPorts to numPorts-1 generate	
-		outputBufferFifo : fbSwitchFifo
-			port map (
-				rd_clk => clk125,
-				wr_clk => clk125,
-				rst => reset,
-				din => swOutputLinksOut(i).data,
-				wr_en => swOutputLinksOut(i).writeEnable,
-				rd_en => outputBufferIn(i).readEnable,
-				dout => outputBufferOut(i).data,
-				full => swOutputLinksIn(i).full,
-				empty => outputBufferOut(i).empty
-			);
-	end generate generate_output_buffer_fifo;
-	
-	downstream0BufferFifo : fbSwitchFifo
-			port map (
-				rd_clk => downstream0ReadClock,
-				wr_clk => clk125,
-				rst => reset,
-				din => swOutputLinksOut(0).data,
-				wr_en => swOutputLinksOut(0).writeEnable,
-				rd_en => outputBufferIn(0).readEnable,
-				dout => outputBufferOut(0).data,
-				full => swOutputLinksIn(0).full,
-				empty => outputBufferOut(0).empty
-			);
-	
-	downstream1BufferFifo : fbSwitchFifo
-			port map (
-				rd_clk => downstream0ReadClock,
-				wr_clk => clk125,
-				rst => reset,
-				din => swOutputLinksOut(1).data,
-				wr_en => swOutputLinksOut(1).writeEnable,
-				rd_en => outputBufferIn(1).readEnable,
-				dout => outputBufferOut(1).data,
-				full => swOutputLinksIn(1).full,
-				empty => outputBufferOut(1).empty
-			);
-	
-	
-	-----------------------------------------------------------------
-	-- OUTPUT TO RING
-	-----------------------------------------------------------------
-	
-	ring_out_generate : for i in numPorts-1 downto numIntPorts generate
-		ringOutputEmpty(i-numIntPorts) <= outputBufferOut(i).empty;
-		ringOutputData((i-numIntPorts+1)*(dataWidth+1)-1 downto (i-numIntPorts)*(dataWidth+1)) <= outputBufferOut(i).data;
-		outputBufferIn(i).readEnable <= ringOutputReadEnable(i-numIntPorts);
-	end generate;
-	
-	-----------------------------------------------------------------
-	-- DOWNSTREAM TO FUNCTIONAL BLOCK
-	-----------------------------------------------------------------
-	
-	downstream0Empty <= outputBufferOut(0).empty;
-	downstream0Data <= outputBufferOut(0).data;
-	outputBufferIn(0).readEnable <= downstream0ReadEnable;
-	
-	downstream1Empty <= outputBufferOut(1).empty;
-	downstream1Data <= outputBufferOut(1).data;
-	outputBufferIn(1).readEnable <= downstream0ReadEnable;
 	
 end architecture rtl;
